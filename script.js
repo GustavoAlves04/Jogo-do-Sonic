ca7f36d8c72fd5786e14404a06402e95b6bb6281
GustavoAlves04 <gustavoalcontato@gmail.com>
2023-05-25 23:47:24 -0300

    sonic game

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const sonic = document.querySelector('.sonic')
-const bola = document.querySelector('.bola-de-ferro')
+const sonic = document.querySelector('.sonic');
+const bola = document.querySelector('.bola-de-ferro');
 
 
 const jump = () => {
-    sonic.classList.add('jump')
+    sonic.classList.add('jump');
 
     setTimeout(()=>{
-        sonic.classList.remove('jump')
-    },500)
+        sonic.classList.remove('jump');
+    },500);
     
 }
 
 const loop = setInterval (()=>{
 
-    const bolaPosition = bola.offsetLeft
-    const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '')
+    const bolaPosition = bola.offsetLeft;
+    const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
 
     if(bolaPosition <= 140 && bolaPosition > 0 && sonicPosition < 80){
-        bola.style.animation = 'none'
-        bola.style.left = `${bolaPosition}px`
+        bola.style.animation = 'none';
+        bola.style.left = `${bolaPosition}px`;
 
-        sonic.style.animation = 'none'
-        sonic.style.bottom = `${sonicPosition}px`
+        sonic.style.animation = 'none';
+        sonic.style.bottom = `${sonicPosition}px`;
 
-        sonic.src = './img/sonic-gameover.gif'
-        sonic.style.width = '100px'
-        sonic.style.marginLeft = '50px'
+        sonic.src = './img/sonic-gameover.gif';
+        sonic.style.width = '100px';
+        sonic.style.marginLeft = '50px';
 
 
-        clearInterval(loop)
+        clearInterval(loop);
     }
 
-},10)
+},10);
 
-document.addEventListener('keydown', jump)
+document.addEventListener('keydown', jump);
