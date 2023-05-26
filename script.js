c2fee8f712b6cdbefa3cd0488253d7c3467cc412
GustavoAlves04 <gustavoalcontato@gmail.com>
2023-05-26 00:08:17 -0300

    sonic game

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 const sonic = document.querySelector('.sonic');
 const bola = document.querySelector('.bola-de-ferro');
 
-
 const jump = () => {
-    sonic.classList.add('jump');
+  sonic.classList.add('jump');
 
-    setTimeout(()=>{
-        sonic.classList.remove('jump');
-    },500);
-    
-}
+  setTimeout(() => {
+    sonic.classList.remove('jump');
+  }, 500);
 
-const loop = setInterval (()=>{
+  if (sonic.classList.add('jump')) {
+    sonic.src = './img/sonic-rolling.gif';
+  }
+};
 
-    const bolaPosition = bola.offsetLeft;
-    const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+const loop = setInterval(() => {
+  const bolaPosition = bola.offsetLeft;
+  const sonicPosition = +window
+    .getComputedStyle(sonic)
+    .bottom.replace('px', '');
 
-    if(bolaPosition <= 140 && bolaPosition > 0 && sonicPosition < 80){
-        bola.style.animation = 'none';
-        bola.style.left = `${bolaPosition}px`;
+  if (bolaPosition <= 140 && bolaPosition > 0 && sonicPosition < 120) {
+    bola.style.animation = 'none';
+    bola.style.left = `${bolaPosition}px`;
 
-        sonic.style.animation = 'none';
-        sonic.style.bottom = `${sonicPosition}px`;
+    sonic.style.animation = 'none';
+    sonic.style.bottom = `${sonicPosition}px`;
 
-        sonic.src = './img/sonic-gameover.gif';
-        sonic.style.width = '100px';
-        sonic.style.marginLeft = '50px';
+    sonic.src = './img/sonic-gameover.gif';
+    sonic.style.width = '100px';
+    sonic.style.marginLeft = '50px';
 
-
-        clearInterval(loop);
-    }
-
-},10);
+    clearInterval(loop);
+  }
+}, 10);
 
 document.addEventListener('keydown', jump);
